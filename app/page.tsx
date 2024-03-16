35ad103b693afcebad6edb6a511f7741e4a1a3cf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-16 20:16:52 +0300

    Some text buttona

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,19 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
 	return (
-		<main className={'flex flex-col gap-2 max-w-[200px]'}>
+		<main className={'flex flex-col gap-2 max-w-[300px]'}>
 			<Button className={'m-5'}>Default</Button>
 			<Button className={'m-5'} variant={'primary'}>
 				Primary
 			</Button>
 			<Button className={'m-5'} variant={'primaryOutline'}>
 				Primary Outline
+			</Button>
+			<Button className={'m-5'} variant={'secondary'}>
+				Secondary
+			</Button>
+			<Button className={'m-5'} variant={'secondaryOutline'}>
+				Secondary Outline
 			</Button>
 		</main>
 	)
