dd3079a2dcc27b7dc357449843ce8edddf239ac9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-16 20:21:01 +0300

    Added the danger and dangerOutline buttons

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,13 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
 	return (
 		<main className={'flex flex-col gap-2 max-w-[300px]'}>
-			<Button className={'m-5'}>Default</Button>
-			<Button className={'m-5'} variant={'primary'}>
-				Primary
-			</Button>
-			<Button className={'m-5'} variant={'primaryOutline'}>
-				Primary Outline
-			</Button>
-			<Button className={'m-5'} variant={'secondary'}>
-				Secondary
-			</Button>
-			<Button className={'m-5'} variant={'secondaryOutline'}>
-				Secondary Outline
-			</Button>
+			<Button>Default</Button>
+			<Button variant={'primary'}>Primary</Button>
+			<Button variant={'primaryOutline'}>Primary Outline</Button>
+			<Button variant={'secondary'}>Secondary</Button>
+			<Button variant={'secondaryOutline'}>Secondary Outline</Button>
+			<Button variant={'danger'}>Danger</Button>
+			<Button variant={'dangerOutline'}>Danger Outline</Button>
 		</main>
 	)
 }
