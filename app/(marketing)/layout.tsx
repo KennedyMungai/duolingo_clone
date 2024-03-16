d32c7b4210196b9dbb94de69f1409b896538c2e6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-17 02:40:49 +0300

    Added some styles for the marketing layout file

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,11 +1,11 @@
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react'
 
 type Props = {
 	children: ReactNode
 }
 
 const MarketingLayout = ({ children }: Props) => {
-	return <div>{children}</div>
+	return <div className='min-h-screen flex flex-col'>{children}</div>
 }
 
 export default MarketingLayout
