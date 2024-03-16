83da8823f947e230b9ba71fd66d6594e04ef1921
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-17 02:33:27 +0300

    Got rid of all markup

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,3 @@
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
-	return (
-		<main className={'flex flex-col gap-2 max-w-[300px]'}>
-			<Button>Default</Button>
-			<Button variant={'primary'}>Primary</Button>
-			<Button variant={'primaryOutline'}>Primary Outline</Button>
-			<Button variant={'secondary'}>Secondary</Button>
-			<Button variant={'secondaryOutline'}>Secondary Outline</Button>
-			<Button variant={'danger'}>Danger</Button>
-			<Button variant={'dangerOutline'}>Danger Outline</Button>
-			<Button variant={'super'}>Super</Button>
-			<Button variant={'superOutline'}>Super Outline</Button>
-			<Button variant={'ghost'}>Ghost</Button>
-			<Button variant={'sidebar'}>Sidebar</Button>
-			<Button variant={'sidebarOutline'}>Sidebar Outline</Button>
-		</main>
-	)
+	return <main></main>
 }
