40955ac853d00475728aea5819c17dd48b9b5705
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-16 20:09:31 +0300

    Added some button variants and tested them out

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,13 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
 	return (
 		<main className={'flex flex-col gap-2 max-w-[200px]'}>
-			<Button className={'m-5'}>Something</Button>
-			<Button className={'m-5'}>Something Else</Button>
+			<Button className={'m-5'}>Default</Button>
+			<Button className={'m-5'} variant={'primary'}>
+				Primary
+			</Button>
+			<Button className={'m-5'} variant={'primaryOutline'}>
+				Primary Outline
+			</Button>
 		</main>
 	)
 }
