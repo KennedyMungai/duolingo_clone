b89086672336f538c4da481e3d86bd1b8aeb1832
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-17 14:07:11 +0300

    Added some markup and styles for the component

--- app/(main)/learn/header.tsx
+++ app/(main)/learn/header.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 type Props = {
 	title: string
 }
 
 const Header = ({ title }: Props) => {
-	return <div>{title}</div>
+	return (
+		<div className='sticky top-0 bg-white pb-3 lg:pt-[28px] flex items-center justify-between border-b-2 mb-2 text-neutral-400 lg:z-50'>
+			<Link href=''>
+				<Button>{title}</Button>
+			</Link>
+		</div>
+	)
 }
 
 export default Header
