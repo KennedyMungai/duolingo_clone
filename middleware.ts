5b796f823825a60fcff6b48844e1ba9da4ac58f5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-17 03:09:06 +0300

    Made some changes

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-	// Routes that can be accessed while signed out
 	publicRoutes: ['/'],
-	// Routes that can always be accessed, and have
-	// no authentication information
-	ignoredRoutes: ['/']
 })
 
 export const config = {
