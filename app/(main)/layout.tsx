2a9d02ad62c7e4b81bb6a76cf4cc0bf594af3f5b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-17 11:37:01 +0300

    Created and rendered out the mobile-header component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import MobileHeader from '@/components/mobile-header'
 import SideBar from '@/components/sidebar'
 import { ReactNode } from 'react'
 
@@ -8,6 +9,7 @@ type Props = {
 const MainLayoutPage = ({ children }: Props) => {
 	return (
 		<>
+			<MobileHeader />
 			<SideBar className='hidden lg:flex' />
 			<main className='lg:pl-[256px] h-full pt-[50px] lg:pt-0'>
 				<div className='bg-red-500 h-full'>{children}</div>
---
+++ components/mobile-header.tsx
@@ -0,0 +1,11 @@
+type Props = {}
+
+const MobileHeader = (props: Props) => {
+	return (
+		<div className='lg:hidden px-6 flex items-center bg-yellow-500 border-b fixed top-0 w-full z-50 h-[50px]'>
+			MobileHeader
+		</div>
+	)
+}
+
+export default MobileHeader
