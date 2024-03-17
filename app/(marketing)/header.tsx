a89214200c6fc28f7f328e27dfe4ccdc59e87ed9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-17 03:23:29 +0300

    Rendered out the logo

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image'
+
 const Header = () => {
 	return (
 		<header className='h-20 w-full border-b-2 border-slate-200 px-4'>
-			Header
+			<div className='max-w-screen-lg mx-auto flex items-center justify-between h-full'>
+				<div className='pt-8 pl-4 pb-7 flex items-center gap-x-3'>
+					<Image
+						src={'/mascot.svg'}
+						width={48}
+						height={48}
+						alt={'Mascot'}
+					/>
+				</div>
+			</div>
 		</header>
 	)
 }
