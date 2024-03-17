b316eab04411611468c3e9c636df2af987fe7629
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-17 13:08:45 +0300

    Removed the background color

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -9,7 +9,7 @@ const SideBar = ({ className }: Props) => {
 	return (
 		<aside
 			className={cn(
-				'flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col',
+				'flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col',
 				className
 			)}
 		>
