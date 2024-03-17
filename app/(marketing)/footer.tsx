3c7e4d0956e4c515c655b5e80958bd1e8012642e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-17 10:58:08 +0300

    Moved the classes to the images

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -5,16 +5,13 @@ const Footer = () => {
 	return (
 		<footer className='hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2'>
 			<div className='max-w-screen-lg mx-auto flex items-center justify-evenly h-full'>
-				<Button
-					size={'lg'}
-					variant={'ghost'}
-					className='w-full flex gap-2'
-				>
+				<Button size={'lg'} variant={'ghost'} className='w-full'>
 					<Image
 						src='/KE.svg'
 						alt='Kiswahili'
 						height={32}
 						width={40}
+						className='mr-4 rounded-md'
 					/>
 					Kiswahili
 				</Button>
