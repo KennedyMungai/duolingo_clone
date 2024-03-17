c9933c1e3416dd1a7b6d69ad8f729a8fcbdd5de0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-17 13:34:45 +0300

    Added the UserButton component to the SideBar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,8 @@ import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import SidebarItem from './sidebar-item'
+import { ClerkLoaded, ClerkLoading, UserButton } from '@clerk/nextjs'
+import { Loader } from 'lucide-react'
 
 type Props = {
 	className?: string
@@ -49,6 +51,14 @@ const SideBar = ({ className }: Props) => {
 					iconSrc={'/shop.svg'}
 					href={'/shop'}
 				/>
+			</div>
+			<div className='p-4'>
+				<ClerkLoading>
+					<Loader className='w-5 h-5 text-muted-foreground animate-spin' />
+				</ClerkLoading>
+				<ClerkLoaded>
+					<UserButton afterSignOutUrl='/' />
+				</ClerkLoaded>
 			</div>
 		</aside>
 	)
