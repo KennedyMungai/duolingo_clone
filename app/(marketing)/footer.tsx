76a2164a34a866ee4e1909b6225cf8b5efd9ddb0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-17 10:56:52 +0300

    Added a button at the footer to learn kiswahili

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,24 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
 const Footer = () => {
 	return (
 		<footer className='hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2'>
-			Footer
+			<div className='max-w-screen-lg mx-auto flex items-center justify-evenly h-full'>
+				<Button
+					size={'lg'}
+					variant={'ghost'}
+					className='w-full flex gap-2'
+				>
+					<Image
+						src='/KE.svg'
+						alt='Kiswahili'
+						height={32}
+						width={40}
+					/>
+					Kiswahili
+				</Button>
+			</div>
 		</footer>
 	)
 }
