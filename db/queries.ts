ba6db801d030c1a69041df47b426d331a3f011c0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-19 02:23:17 +0300

    Gave the courses a static type

--- db/queries.ts
+++ db/queries.ts
@@ -2,5 +2,9 @@ import { cache } from 'react'
 import { db } from './drizzle'
 
 export const getCourses = cache(async () => {
-	return await db.query.courses.findMany()
+	const courses: Course[] = await db.query.courses.findMany()
+
+	console.log(JSON.stringify(courses))
+
+	return courses
 })
