7da076e125017c1745515887cac817c17ddb2cda
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-19 02:39:13 +0300

    Created and rendered out the card components

---
+++ app/(main)/courses/card.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+	id: string
+	title: string
+	imageSrc: string
+	onClick: () => void
+	disabled: boolean
+	active: boolean
+}
+
+const Card = ({ active, disabled, id, imageSrc, onClick, title }: Props) => {
+	return <div>Card</div>
+}
+
+export default Card
--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -1,12 +1,28 @@
 'use client'
 
+import Card from "./card"
+
 type Props = {
 	courses: Course[]
 	activeCourseId: string
 }
 
 const List = ({ activeCourseId, courses }: Props) => {
-	return <div>List</div>
+	return (
+		<div className='pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4'>
+			{courses.map((course) => (
+				<Card
+					key={course.id}
+					id={course.id}
+					title={course.title}
+                    imageSrc={course.imageSrc}
+                    onClick={() = {}}
+                    disabled={false}
+                    active={course.id === activeCourseId}
+				/>
+			))}
+		</div>
+	)
 }
 
 export default List
