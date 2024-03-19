b0f13d9359581ab3982e78cfba1232e605dbc25d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-19 03:02:05 +0300

    Created the userProgress schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,18 @@
-import { pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { integer, pgTable, text, uuid } from 'drizzle-orm/pg-core'
 
 export const courses = pgTable('courses', {
 	id: uuid('id').defaultRandom().primaryKey(),
 	title: text('title').notNull(),
 	imageSrc: text('image_src').notNull()
+})
+
+export const userProgress = pgTable('user_progress', {
+	userId: text('user_id').primaryKey(),
+	userName: text('user_name').notNull().default('User'),
+	userImageSrc: text('user_image_src').notNull().default('/mascot.svg'),
+	activeCourseId: uuid('active_course_id').references(() => courses.id, {
+		onDelete: 'cascade'
+	}),
+	hearts: integer('hearts').notNull().default(5),
+	points: integer('points').notNull().default(0)
 })
